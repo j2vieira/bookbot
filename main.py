301de19b2d1a69decec66c449c144a52a4625cd3
jamvieira <joao.stuxz@gmail.com>
2024-09-18 14:24:03 -0700

    done

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-    print(chars_dict)
+    sorted_dict = sort_dict(chars_dict)
+    report(sorted_dict, num_words)
 
 
 def get_num_words(text):
@@ -26,6 +27,23 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def sort_dict(dict):
+    list = []
+    for key, value in dict.items():
+        if key.isalpha():
+            list.append((key, value))
+
+    sorted_list = sorted(list, reverse = True, key = lambda item:item[1])
+    return sorted_list
+
+def report(list,num_words):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document")
+
+    for key, value in list:
+        print(f"The '{key}' character was found {value} times")
+
+    print("--- End report ---")
 
 main()
 
